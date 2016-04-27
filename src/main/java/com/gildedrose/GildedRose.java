089a3d91170fac644f33cbed81162792ed17c181
Stephan M. February <stephan.february@gmail.com>
2016-04-27 20:02:55 +0800

    First pass at all green

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,57 +7,92 @@ class GildedRose {
         this.items = items;
     }
 
+    private boolean isAgedBrie(Item item){
+        return item.name.equals("Aged Brie");
+    }
+
+    private boolean isBackstagePass(Item item){
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            //handle products whose quality is affected by age
+            if (isAgingProduct(items[i])) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                    decreaseQuality(items[i]) ;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    increaseQuality(items[i]);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isBackstagePass(items[i])) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQuality(items[i]);
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQuality(items[i]);
                         }
                     }
-                }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            //no effect by age on quality of this item
+            if (!isSulfuras(items[i])) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
+            updateExpiredItems(items[i]);
+        }
+    }
+
+    private boolean isAgingProduct(Item item) {
+        return !isAgedBrie(item) && !isBackstagePass(item);
+    }
+
+    private void updateExpiredItems(Item item) {
+        if (item.sellIn < 0) {
+            if (isAgedBrie(item)) {
+                increaseQuality(item);
+            } else {
+
+                if (isBackstagePass(item)) {
+                   zeroItemQuality(item) ;
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality > 0) {
+                        decreaseQuality(item);
                     }
                 }
             }
         }
     }
+
+    private void zeroItemQuality(Item item) {
+        item.quality = item.quality - item.quality;
+    }
+
+    private void decreaseQuality(Item item) {
+
+        if (!isSulfuras(item)) {
+            item.quality = item.quality - 1;
+        }
+
+        if (isConjuredProduct(item)){
+            item.quality = item.quality - 1; //additional decrement for conjured products
+        }
+    }
+
+    private boolean isConjuredProduct(Item item) {
+        return item.name.equals("Conjured Mana Cake");
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
 }
